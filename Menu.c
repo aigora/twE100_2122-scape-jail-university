8f86c4b38a4435d9ba5e3049540aae8c1c0b8ef9
fernandoriverob <101277816+fernandoriverob@users.noreply.github.com>
2022-05-23 12:11:49 +0200

    Mejoras.
    A la hora de que pueda tener fallos el programa cuando metes una letra en lugar de un numero por error, se ha solventado en la parte de que se podía bloquear en un bucle infinito. No se ha podido con el valor de la edad o con el numero de participantes, ya que tiene que ser números obligatoriamente.

--- Menu.c
+++ Menu.c
@@ -8,7 +8,7 @@ typedef struct
     char apellido1[20];
     char apellido2[20];
     int edad;
-    int work;
+    char work;
 }integrante;
 
 void menu(int jugadores)
@@ -24,84 +24,81 @@ void menu(int jugadores)
 
         printf("\tIntroduzca el nombre del equipo.\n");
         scanf(" %30[^\n]", equipo);
-        printf("\t¿Entonces el nombre del equipo sera %s?\n", equipo);
+        printf("\tÂ¿Entonces el nombre del equipo sera %s?\n", equipo);
         printf("\tEscriba el numero y dele al intro:\n\t1)Si\n\t2)No\n");
         scanf("%d", &a);
     }
-    while(a==2);
+    while(a != 49);
     system("cls");
-//    do
-//    {
-        printf("\tDecirme cuantos integrantes sois.\n");
-        scanf("%d", &n);
-//        if()
-//    }
-//    while();
+    
+    printf("\tDecirme cuantos integrantes sois.\n");
+    scanf("%d", &n);
+    
     integrante jugador[n];
     for (i=0; i<n; i++)
     {
         do
         {
             printf("\tNombre: \t");
-            scanf("%20s", &jugador[i].nombre);
+            scanf("%20[^\n]", &jugador[i].nombre);
             printf("\n\tPrimer apellido: \t");
-            scanf("%20s", &jugador[i].apellido1);
+            scanf("%20[^\n]", &jugador[i].apellido1);
             printf("\n\tSegundo apellido: \t");
-            scanf("%20s", &jugador[i].apellido2);
+            scanf("%20[^\n]", &jugador[i].apellido2);
             do
             {
                 printf("\n\tEdad: \t");
                 scanf("%d", &jugador[i].edad);
                 if((jugador[i].edad<1)&&(jugador[i].edad>100))
                 {
-                    printf("\tPor favor, escriba su edad en número.\n");
+                    printf("\tPor favor, escriba su edad en nÃºmero.\n");
                 }
             }
             while((jugador[i].edad<1)&&(jugador[i].edad>100));
             do
             {
-                printf("\n\t¿Usted esta trabajando o esta estudiando? Marca el numero correspondiente:\n\t1)Estudiando\n\t2)Trabajando\n\t3)Ambas\n\t4)Ninguna de las anteriores.\n");
-                scanf("%d", &jugador[i].work);
-                if((jugador[i].work<1)&&(jugador[i].work>5))
+                printf("\n\tÂ¿Usted esta trabajando o esta estudiando? Marca el numero correspondiente:\n\t1)Estudiando\n\t2)Trabajando\n\t3)Ambas\n\t4)Ninguna de las anteriores.\n");
+                scanf(" %c", &jugador[i].work);
+                if((jugador[i].work != 49)&&(jugador[i].work != 50)&&(jugador[i].work != 51)&&(jugador[i].work != 52))
                 {
                     printf("\tPor favor, escriba el valor numerico.\n");
                 }
             }
-            while((jugador[i].work<1)&&(jugador[i].work>100));
+            while((jugador[i].work != 49)&&(jugador[i].work != 50)&&(jugador[i].work != 51)&&(jugador[i].work != 52));
 
-            printf("\t¿Entonces estos son sus datos?\n");
+            printf("\tÂ¿Entonces estos son sus datos?\n");
             printf("\tNombre: %s\n", jugador[i].nombre);
             printf("\tPrimer apellido: %s\n", jugador[i].apellido1);
             printf("\tSegundo apellido: %s\n", jugador[i].apellido2);
             printf("\tEdad: %d\n", jugador[i].edad);
-            if(jugador[i].work==1)
+            if(jugador[i].work==49)
             {
                 printf("\tEstudiante.\n");
             }
-            else if(jugador[i].work==2)
+            else if(jugador[i].work==50)
             {
                 printf("\tTrabajador.\n");
             }
-            else if(jugador[i].work==3)
+            else if(jugador[i].work==51)
             {
                 printf("\tTrabajador y estudiante a la vez.\n");
             }
-            else if(jugador[i].work==4)
+            else if(jugador[i].work==52)
             {
                 printf("\tNi estudiante, ni trabajador.\n");
             }
             printf("\tMarque una de las respuestas:\n\t1)Si\n\t2)No\n");
             scanf("%d", &b);
-            if((b != 1)&&(b != 2))
+            if((b != 49)&&(b != 50))
             {
-                    printf("\tIntroduzca el valor numérico.\n");
+                    printf("\tIntroduzca el valor numÃ©rico.\n");
             }
-            if(b==2)
+            if(b != 49)
             {
                 printf("\tIntroduzca de nuevo sus datos.\n");
             }
         }
-        while(b==2);
+        while(b != 49);
         system("cls");
     }
     printf("\n");
