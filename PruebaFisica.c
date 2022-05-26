f468e845fb724a1366f4fc807f66fa81dc82c969
fernandoriverob <101277816+fernandoriverob@users.noreply.github.com>
2022-05-26 14:31:39 +0200

    Tenga guardado de partida.
    Adición del guardado de partida.

--- PruebaFisica.c
+++ PruebaFisica.c
@@ -11,6 +11,8 @@ int pruebaFisica(float modo)
     int pruebaFisica;
     //Generador aleatorio.
     int n1, n2, n3;
+    FILE *pf;
+    
     srand(time(NULL));
     n1=((double)rand()/RAND_MAX)*(3-1)+1;
     n2=((double)rand()/RAND_MAX)*(3-1)+1;
@@ -32,7 +34,7 @@ int pruebaFisica(float modo)
         do
         {
             //Opcion 1
-            printf("\tLa primera pregunta: ¿Que pesa mas un kilo de paja o un kilo de acero?\n");
+            printf("\tLa primera pregunta: Â¿Que pesa mas un kilo de paja o un kilo de acero?\n");
             printf("\tLas soluciones son:\n\t1)Pesa mas un kilo de paja\n\t2)Pesa mas un kilo de acero\n\t3)Ninguna de las anteriores son correctas\n");
             printf("\tSolucion 1, 2 o 3\n");
             scanf("%d", &p1);
@@ -80,13 +82,13 @@ int pruebaFisica(float modo)
 
     }
     break;
-    //Segunda opción
+    //Segunda opciÃ³n
     case 2:
     {
         do
         {
-            //Opciòn 2
-            printf("\tPrimera pregunta, ¿Cual tiene más velocidad un coche azul que va a 25 metros por segundo o otro rojo que va a 90 kilometros hora?.\n");
+            //OpciÃ²n 2
+            printf("\tPrimera pregunta, Â¿Cual tiene mÃ¡s velocidad un coche azul que va a 25 metros por segundo o otro rojo que va a 90 kilometros hora?.\n");
             printf("\tLas soluciones son:\n\t1)El coche azul.\n\t2)El coche rojo.\n\t3)Ninguna de las anteriores.\n");
             printf("\tSolucin 1, 2 o 3.\n");
             scanf("%d", &p1);
@@ -118,7 +120,7 @@ int pruebaFisica(float modo)
                 break;
             }
         }
-        //Solo vale número de 1 a 3
+        //Solo vale nÃºmero de 1 a 3
         while((p1<1) || (p1>3));
         //Comprobacion
         if (p1 == 3)
@@ -133,13 +135,13 @@ int pruebaFisica(float modo)
         }
     }
     break;
-    //Tercera opción
+    //Tercera opciÃ³n
     case 3:
         {
             do
             {
-                //Opción 3
-                printf("\tPrimera pregunta. ¿Cuáles son las unidades del sistema internacional de la energía?\n");
+                //OpciÃ³n 3
+                printf("\tPrimera pregunta. Â¿Cuales son las unidades del sistema internacional de la energÃ­a?\n");
                 printf("\tLas soluciones son:\n\t1)Voltios.\n\t2)Amperios.\n\t3)Ninguna de las anteriores.\n");
                 printf("\tSolucion 1, 2 o 3.\n");
                 scanf("%d", &p1);
@@ -171,9 +173,9 @@ int pruebaFisica(float modo)
                 break;
                 }
         }
-        //Para que solo haya números
+        //Para que solo haya nÃºmeros
         while((p1<1) || (p1>3));
-        //Comprobación
+        //ComprobaciÃ³n
         if (p1 == 3)
         {
             //Correcto
@@ -202,7 +204,7 @@ int pruebaFisica(float modo)
     case 1:
         {
             float tiempo1, tiempo2;
-            printf("\tSegunda pregunta: ¿Donde estara un tren que sale desde Madrid hasta Santander con una velocidad inicial de 25 metros/segundos, una aceleracion de 0.05 metros por segundo durante media hora, si el tiempo transcurrido son 2 horas?\n");
+            printf("\tSegunda pregunta: Â¿Donde estara un tren que sale desde Madrid hasta Santander con una velocidad inicial de 25 metros/segundos, una aceleracion de 0.05 metros por segundo durante media hora, si el tiempo transcurrido son 2 horas?\n");
             printf("\tLa solucion en kilometros.\n");
             scanf("%f", &p2);
             sol = posicion (tiempo1, tiempo2);
@@ -219,7 +221,7 @@ int pruebaFisica(float modo)
     case 2:
         {
             float velocidad, masa;
-            printf("\tSegunda pregunta: ¿Cual sera la energia cinetica que desprende un cuerpo que tiene una masa de quinientos gramos y un velocidad estable de setetenta y dos kilometros por hora?\n");
+            printf("\tSegunda pregunta: Â¿Cual sera la energia cinetica que desprende un cuerpo que tiene una masa de quinientos gramos y un velocidad estable de setetenta y dos kilometros por hora?\n");
             printf("\tLa solucion en Julios.\n");
             scanf("%f", &p2);
             sol = energia (masa, velocidad);
@@ -236,7 +238,7 @@ int pruebaFisica(float modo)
     case 3:
         {
             float masa2, aceleracion;
-            printf("\tSegunda pregunta: ¿Cual sera la fuerza a aplicar sobre un cuerpo de masa tres mil quinientos gramos y se lo queremos mover a dieciocho (km/h^2)");
+            printf("\tSegunda pregunta: Â¿Cual sera la fuerza a aplicar sobre un cuerpo de masa tres mil quinientos gramos y se lo queremos mover a dieciocho (km/h^2)");
             printf("\tLa solucion en Newtons.\n");
             scanf("%f", &p2);
             sol = fuerza (masa2, aceleracion);
@@ -264,7 +266,7 @@ int pruebaFisica(float modo)
     case 1:
         {
             printf("\tTenemos este acertijo: 'Cuanto mas gorda esta mayor es el amor.'\n");
-            printf("\tEsto significa:\n\tA)Los protones siempre se acercan a los neutrones.\n\tB)A mayor volumen, mayor capacidad.\n\tC)Cuanto más peso, mayor fuerza de atraccion.\n");
+            printf("\tEsto significa:\n\tA)Los protones siempre se acercan a los neutrones.\n\tB)A mayor volumen, mayor capacidad.\n\tC)Cuanto mÃ¡s peso, mayor fuerza de atraccion.\n");
             scanf(" %c", &letter);
             if ((letter == 67) || (letter == 99))
             {
@@ -279,7 +281,7 @@ int pruebaFisica(float modo)
     break;
     case 2:
         {
-            printf("\tTenemos una duda, ¿Quien dedujo la ley de la gravedad?\n");
+            printf("\tTenemos una duda, Â¿Quien dedujo la ley de la gravedad?\n");
             printf("\tLas soluciones son:\n\tA)Newton.\n\tB)Einstein.\n\tC)Pitagoras.\n");
             scanf(" %c", &letter);
             if ((letter == 65) || (letter == 97))
@@ -295,7 +297,7 @@ int pruebaFisica(float modo)
     break;
     case 3:
         {
-            printf("\t¿En que unidades se define peso (S.I.)?\n");
+            printf("\tÂ¿En que unidades se define peso (S.I.)?\n");
             printf("\tElija una de las soluciones:\n\tA)Kilogramos\n\tB)Newtons\n\tC)Kilogramos*kilometros/segundos.\n");
             scanf(" %c", &letter);
             if ((letter == 66) || (letter == 98))
@@ -330,6 +332,9 @@ int pruebaFisica(float modo)
 
     printf("\n");
     system("cls");
+    pf=fopen("Misdatos.txt", "a");
+    fprintf(pf, "p1 = %d", pruebaFisica);
+    fclose(pf);
     return pruebaFisica;
 }
 
